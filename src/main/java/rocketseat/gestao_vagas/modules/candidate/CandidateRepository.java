642a14fa9e51762b2052c10853891e2d15dc7014
jhonatan <jhonatandev@outlook.com>
2025-03-20 19:22:05 -0300

    security: criptografia e validação da senha do candidato usando passwordEncoder e criação do token de autenticação do candidato.

--- src/main/java/rocketseat/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/rocketseat/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -9,4 +9,6 @@ public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID
     //sem o optional ficaria assim: CandidateEntity findByUs...resto do mmetodo.. ele retornaria ou o objeto ou null
     //com o optional, queremos realizar mais operações
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+
+    Optional<CandidateEntity> findByUsername(String username);
 }
