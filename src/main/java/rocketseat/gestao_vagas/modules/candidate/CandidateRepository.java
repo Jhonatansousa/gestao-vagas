3f91b40eae7c7eb63d1a4d0d8b582590ce3cab2f
Jhonatan <jhonatandev@outlook.com>
2025-10-04 19:20:05 -0300

    feat: add observability through spring actuator & prometheus

--- src/main/java/rocketseat/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/rocketseat/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -1,10 +1,12 @@
 package rocketseat.gestao_vagas.modules.candidate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
     //sem o optional ficaria assim: CandidateEntity findByUs...resto do mmetodo.. ele retornaria ou o objeto ou null
     //com o optional, queremos realizar mais operações
